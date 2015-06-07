be7a8d77c1667d1c478689cec36e23cc05cf5688
MarcioPorto <mflporto@gmail.com>
2015-06-07 16:27:27 -0300

    Initial commit

---
+++ app/src/main/java/com/marcioporto/android/stormy/CurrentWeather.java
@@ -0,0 +1,61 @@
+package com.marcioporto.android.stormy;
+
+/**
+ * Created by Marcio on 6/7/2015.
+ */
+public class CurrentWeather {
+    private String mIcon;
+    private long mTime;
+    private double mTemperature;
+    private double mHumidity;
+    private double mPrecipitationChance;
+    private String mSummary;
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public double getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public double getHumidity() {
+        return mHumidity;
+    }
+
+    public void setHumidity(double humidity) {
+        mHumidity = humidity;
+    }
+
+    public double getPrecipitationChance() {
+        return mPrecipitationChance;
+    }
+
+    public void setPrecipitationChance(double precipitationChance) {
+        mPrecipitationChance = precipitationChance;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+}
